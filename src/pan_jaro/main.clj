73e02150fa49061bd9560decdc5edb5bb29d2bf3
rotaliator <rotaliator@gmail.com>
2021-07-21 21:48:38 +0200

    wersja z transducerem i bez

--- src/pan_jaro/main.clj
+++ src/pan_jaro/main.clj
@@ -1,7 +1,7 @@
 (ns pan-jaro.main
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]
-            [clj-fuzzy.jaro-winkler :refer [jaro jaro-winkler]]))
+  (:require [clojure.string :as str]
+            [clojure.pprint :refer [pprint]]
+            [clj-fuzzy.jaro-winkler :refer [jaro-winkler]]))
 
 (def istotne-słowa (as-> "pan-tadeusz1.txt" $
                      (slurp $)                                     ;; odczyt z pliku
@@ -10,12 +10,55 @@
 
 (def wybrane-słowa (repeatedly 30 #(rand-nth istotne-słowa)))
 
-(comment
-  (jaro "MARTHA" "MARHTA")
-  (jaro "DIXON" "DICKSONX")
-  (jaro "JELLYFISH" "SMELLYFISH")
+(defn zapisz-słowa [lista-słów plik]
+  (doseq [słowo lista-słów]
+    (spit plik słowo :append true)
+    (spit plik "\n" :append true)))
 
-  (jaro-winkler "MARTHA" "MARHTA")
-  (jaro-winkler "DIXON" "DICKSONX")
-  (jaro-winkler "JELLYFISH" "SMELLYFISH")
+(defn odczytaj-słowa [plik]
+  (-> plik slurp str/split-lines))
+
+(defn znajdź-podobne-słowa [słowo słownik minimalne-dopasowanie]
+  (->> słownik
+       (map (fn [s] [s (jaro-winkler słowo s)]))
+       (filter #(> (second %) minimalne-dopasowanie))
+       (sort-by second)
+       (distinct)
+       (reverse)))
+
+(defn znajdź-podobne-słowa-xf
+  "To samo co powyżej tylko z użyciem transducera"
+  [słowo słownik minimalne-dopasowanie]
+  (let [xf          (comp
+                     (map (fn [s] [s (jaro-winkler słowo s)]))
+                     (filter #(> (second %) minimalne-dopasowanie))
+                     (distinct))
+        dopasowania (into [] xf słownik)]
+    (->> dopasowania
+        (sort-by second)
+        (reverse))))
+
+(defn zapisz-dopasowania-słów [słowa słownik minimalne-dopasowanie plik]
+  (doseq [słowo słowa]
+    (let [dopasowania (znajdź-podobne-słowa-xf słowo słownik minimalne-dopasowanie)]
+      (spit plik (str słowo ":\n") :append true)
+      (spit plik (with-out-str (pprint dopasowania)) :append true))))
+
+(comment
+
+  (zapisz-słowa wybrane-słowa "corrections.txt")
+  ;; po zapisie ręcznie modyfikuję słowa
+  (def zmodyfikowane-słowa (odczytaj-słowa "corrections.txt"))
+
+  (znajdź-podobne-słowa "mrówki" istotne-słowa 0.85)
+  ;; => (["mrówek" 0.9333333333333333]
+  ;;     ["mówi" 0.9]
+  ;;     ["Mrówki" 0.888888888888889]
+  ;;     ["mruki" 0.8577777777777779]
+  ;;     ["mów" 0.8500000000000001])
+  (time (znajdź-podobne-słowa "mrówki" istotne-słowa 0.85))
+  (time (znajdź-podobne-słowa-xf "mrówki" istotne-słowa 0.85))
+
+  (time (zapisz-dopasowania-słów zmodyfikowane-słowa istotne-słowa 0.85 "dopasowania.txt"))
+
   )
