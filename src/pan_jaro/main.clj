e8a9e2f2227dec53a11ad0dc12c157f8e17f43aa
Przemyslaw Koza <pkoza@pulawy.com>
2021-07-21 12:30:44 +0200

    Odczyt i losowanie słów

---
+++ src/pan_jaro/main.clj
@@ -0,0 +1,24 @@
+(ns pan-jaro.main
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clj-fuzzy.jaro-winkler :refer [jaro jaro-winkler]]))
+
+(def znaki-spec #"[;.,«»()!:?…*]")
+
+(def istotne-słowa (as-> "pan-tadeusz1.txt" $
+                     (slurp $)                     ;; odczyt z pliku
+                     (str/replace $ znaki-spec "")
+                     (str/split $ #"\s+")          ;; dzielę na wyrazy
+                     (filter #(> (count %) 2) $))) ;; tylko dłuższe niż dwa znaki
+
+(def wybrane-słowa (repeatedly 30 #(rand-nth istotne-słowa)))
+
+(comment
+  (jaro "MARTHA" "MARHTA")
+  (jaro "DIXON" "DICKSONX")
+  (jaro "JELLYFISH" "SMELLYFISH")
+
+  (jaro-winkler "MARTHA" "MARHTA")
+  (jaro-winkler "DIXON" "DICKSONX")
+  (jaro-winkler "JELLYFISH" "SMELLYFISH")
+  )
